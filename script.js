b621e3cc43fd089b08225cf8824cdec27a5d1872
Yan <Yusyuriv@users.noreply.github.com>
2017-05-26 22:20:35 +0700

    Search by right-clicking a link to a game
    Also move license text from README.md to LICENSE

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 var genericSearch = function(info) {
   var query = info.selectionText;
   var section = menuIds[info.menuItemId];
+  var appid;
+  if(info.linkUrl && section.endsWith('-link')) {
+    appid = info.linkUrl.match(/^https?:\/\/(?:store\.steampowered|steamcommunity)\.com\/app\/(\d+).*$/i);
+    if(appid)
+      appid = appid[1];
+    section = section.split('-')[0];
+  }
   var sections = {
     EMOTICONS: 'tag_item_class_4',
     BACKGROUNDS: 'tag_item_class_3',
     CARDS: 'tag_item_class_2',
     BOOSTERS: 'tag_item_class_5'
   };
-  var link = "http://steamcommunity.com/market/search?q=" + (encodeURIComponent(query)) + "&category_753_Game%5B%5D=any" + (sections[section] ? '&category_753_item_class%5B%5D=' + sections[section] : '') + "&appid=753";
+  var link = "http://steamcommunity.com/market/search?q=" +
+    (query ? encodeURIComponent(query) : '') +
+    "&category_753_Game%5B%5D=" +
+    (appid ? 'tag_app_' + appid : 'any') +
+    (sections[section] ? '&category_753_item_class%5B%5D=' + sections[section] : '') +
+    "&appid=753";
   chrome.tabs.create({
     url: link,
     active: false
@@ -15,6 +27,21 @@ var genericSearch = function(info) {
 };
 
 var ctx = ['selection'];
+var linkCtx = ['link'];
+
+var parentSelection = chrome.contextMenus.create({
+  title: 'Search for selection on Community Market',
+  contexts: ctx
+});
+var parentLink = chrome.contextMenus.create({
+  title: 'Search for this link\'s appid on Community Market',
+  contexts: linkCtx,
+  targetUrlPatterns: [
+    '*://store.steampowered.com/app/*',
+    '*://steamcommunity.com/app/*',
+    '*://www.steamcommunity.com/app/*'
+  ]
+});
 
 var menus = [{
   textId: 'EMOTICONS',
@@ -39,7 +66,16 @@ for (var i = 0; i < menus.length; i++) {
   var menuId = chrome.contextMenus.create({
     title: menu.label,
     onclick: genericSearch,
+    parentId: parentSelection,
     contexts: ctx
   });
   menuIds[menuId] = menu.textId;
+
+  menuId = chrome.contextMenus.create({
+    title: menu.label,
+    onclick: genericSearch,
+    parentId: parentLink,
+    contexts: linkCtx
+  });
+  menuIds[menuId] = menu.textId + '-link';
 }
